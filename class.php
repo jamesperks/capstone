802ff708e14fa0741bbc464481b6b1e80e9ef143
James Perks <tail451@gmail.com>
2014-10-01 13:52:59 -0400

    added support for editing classes

--- class.php
+++ class.php
@@ -2,6 +2,27 @@
 require('inc/header.php');
 require('inc/auth.php');
 
+if(isset($_GET['id']) && !isset($_POST['submit'])) {
+    $db = new PDO('mysql:host='.$database['host'].';dbname='.$database['name'].';charset=utf8', $database['user'], $database['pass'], array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $stmt = $db->prepare('SELECT COUNT(*) FROM class WHERE id=? AND studentID=?');
+    $stmt->execute(array($_GET['id'], $_SESSION['userid']));
+    $count = $stmt->fetch();
+    if($count[0][0] == 1) {
+        $stmt = $db->prepare('SELECT * FROM class WHERE id=? AND studentID=? LIMIT 1');
+        $stmt->execute(array($_GET['id'], $_SESSION['userid']));
+        $row = $stmt->fetch();
+        $_POST['name']          = $row['name'];
+        $_POST['location']      = $row['location'];
+        $_POST['day']           = $row['day'];
+        $_POST['startHour']     = date('g', $row['start']);
+        $_POST['startMinute']   = date('i', $row['start']);
+        $_POST['startAMPM']     = ((date('A', $row['start']) == 'PM') ? 2:1);
+        $_POST['endHour']       = date('g', $row['end']);
+        $_POST['endMinute']     = date('i', $row['end']);
+        $_POST['endAMPM']       = ((date('A', $row['end']) == 'PM') ? 2:1);
+    }
+}
+
 function genTestForm ($name=NULL, $location=NULL, $day=NULL, $startHour=NULL, $startMinute=NULL, $startAMPM=NULL, $endHour=NULL, $endMinute=NULL, $endAMPM=NULL) {
     if($name===NULL && $location===NULL && $day===NULL && $startHour===NULL && $startMinute===NULL && $startAMPM===NULL && $endHour===NULL && $endMinute===NULL && $endAMPM===NULL) {
         return '<form role="form" class="addclass-form" method="post" action="class.php">
@@ -52,7 +73,7 @@ function genTestForm ($name=NULL, $location=NULL, $day=NULL, $startHour=NULL, $s
                 </div>
             </form>';
     } else {
-        return '<form role="form" class="addclass-form" method="post" action="class.php">
+        return '<form role="form" class="addclass-form" method="post" action="class.php'.((isset($_GET['id'])) ? '?id='.$_GET['id']:NULL).'">
                 <div class="form-group">
                   <label>Class Name</label>
                   <input type="text" name="name" class="form-control" placeholder="Enter class name" value="'.$name.'">
@@ -96,7 +117,7 @@ function genTestForm ($name=NULL, $location=NULL, $day=NULL, $startHour=NULL, $s
                 </div>
                 <div class="clearfix"></div>
                 <div class="form-group" style="margin-top:20px;">
-                    <input type="reset" class="btn btn-danger btn-lg" value="Clear" /> <input type="submit" name="submit" class="btn btn-primary btn-lg pull-right" value="Add Class" />
+                    <input type="reset" class="btn btn-danger btn-lg" value="Clear" /> '.((isset($_GET['id'])) ? '<a href="hours.php?id='.$_GET['id'].'" class="btn btn-info btn-lg">Study Hours</a>':NULL).' <input type="submit" name="submit" class="btn btn-primary btn-lg pull-right" value="'.((isset($_GET['id'])) ? 'Update':'Add').' Class" />
                 </div>
             </form>';
     }
@@ -148,12 +169,21 @@ if($_POST['submit']) {
             $_POST['name']      = strip_tags(trim($_POST['name']));
             $_POST['location']  = strip_tags(trim($_POST['location']));
             $db = new PDO('mysql:host='.$database['host'].';dbname='.$database['name'].';charset=utf8', $database['user'], $database['pass'], array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            $stmt = $db->prepare("INSERT INTO class(`studentID`,`day`,`start`,`end`,`name`,`location`) VALUES(?,?,?,?,?,?)");
-            $stmt->execute(array($_SESSION['userid'], $_POST['day'], $startTime, $endTime, $_POST['name'], $_POST['location']));
-            $last_id = $db->lastInsertId();
-            //$output = "<span class=\"success bg-success\">You added ".$_POST['name']." to your schedule.</span><br />
-            //<a href=\"hours.php?id=".$last_id."\">Click here to add study and homework hours</a>.</span>";
-            header('Location:hours.php?id='.$last_id);
+            if(!empty($_GET['id'])) {
+                $stmt = $db->prepare("UPDATE class SET day=?, start=?, end=?, name=?, location=? WHERE id=? AND studentID=?");
+                $stmt->execute(array($_POST['day'], $startTime, $endTime, $_POST['name'], $_POST['location'], $_GET['id'], $_SESSION['userid']));
+                $output .= '<span class="success bg-success center-block">Saved</span>';
+                if($_POST['startHour'] > 12) $_POST['startHour'] = ($_POST['startHour']-12);
+                if($_POST['endHour'] > 12) $_POST['endHour'] = ($_POST['endHour']-12);
+                $output .= genTestForm($_POST['name'],$_POST['location'],$_POST['day'],$_POST['startHour'],$_POST['startMinute'],$_POST['startAMPM'],$_POST['endHour'],$_POST['endMinute'],$_POST['endAMPM']);
+            } else {
+                $stmt = $db->prepare("INSERT INTO class(`studentID`,`day`,`start`,`end`,`name`,`location`) VALUES(?,?,?,?,?,?)");
+                $stmt->execute(array($_SESSION['userid'], $_POST['day'], $startTime, $endTime, $_POST['name'], $_POST['location']));
+                $last_id = $db->lastInsertId();
+                //$output = "<span class=\"success bg-success\">You added ".$_POST['name']." to your schedule.</span><br />
+                //<a href=\"hours.php?id=".$last_id."\">Click here to add study and homework hours</a>.</span>";
+                header('Location:hours.php?id='.$last_id);
+            }
         } catch(Exception $e) {
             $output = "<span class=\"error bg-warning\">Something went wrong. We're working on it, sit tight.</span>";
             $output .= $e->getMessage();
@@ -165,6 +195,7 @@ if($_POST['submit']) {
 //print_r($errors);
 //print_r($_POST);
 //print_r($_SESSION);
+$db=null;
 ?>
     <div class="container">
         <div class="s40 center-block">
